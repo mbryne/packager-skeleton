e86f0ee18d731c1813a3f4513dac686925d954f6
Michael Bryne <mbryne@gmail.com>
2020-02-26 09:13:13 +1100

    Added default enabled config

--- config/mypackage.php
+++ config/mypackage.php
@@ -1,5 +1,16 @@
 <?php
 
 return [
-    //
+
+    /*
+    |--------------------------------------------------------------------------
+    | Package Enabled
+    |--------------------------------------------------------------------------
+    |
+    | Determines whether this package is available to the application
+    |
+    */
+
+    'enabled' => env(':uc:package_NOTES', true)
+
 ];
